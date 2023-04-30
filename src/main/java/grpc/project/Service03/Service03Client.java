4cc02c7498aa9491858e523ec45be0ce2cf73401
LauraBGitHub <119501811+LauraBGitHub@users.noreply.github.com>
2023-04-30 18:45:19 +0100

    Update Service03Client.java

--- src/main/java/grpc/project/Service03/Service03Client.java
+++ src/main/java/grpc/project/Service03/Service03Client.java
@@ -1,4 +1,31 @@
 package grpc.project.Service03;
 
+import io.grpc.ManagedChannel; 
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 
+import grpc.project.Service03.IncidentResponseGrpc;
+import grpc.project.Service03.IncidentResponseGrpc.IncidentResponseBlockingStub;
+
+public class Service03Client { 
+	private static IncidentResponseGrpc.IncidentResponseBlockingStub blockingStub;
+	public static void main (String[] args) { 
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9091)
+		.usePlaintext()
+		.build(); 
+		//create the stubs
+		blockingStub = IncidentResponseGrpc.newBlockingStub(channel);
+		blockAccess(); 
+		channel.shutdown();
+	 }
+	public static void blockAccess() {
+		
+		BlockAccessMessage request = BlockAccessMessage.newBuilder().setUserId("E32323").setSecureFlag(false).build(); 
+		String user = "E32323 : "; 
+		BlockAccessResponseMessage response = blockingStub.blockAccess(request); 
+		System.out.println(user + response.getResult());
+	}
+}
 		
