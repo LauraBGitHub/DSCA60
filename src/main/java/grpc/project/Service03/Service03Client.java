6e92b15d9a1dfec911dbc38c808b321c250f7795
LauraBGitHub <119501811+LauraBGitHub@users.noreply.github.com>
2023-04-30 20:37:54 +0100

    Update Service03Client.java

--- src/main/java/grpc/project/Service03/Service03Client.java
+++ src/main/java/grpc/project/Service03/Service03Client.java
@@ -5,25 +5,35 @@ import io.grpc.ManagedChannelBuilder;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
-
+//implementation and GUI imports 
 import java.util.Random;
+import javax.swing.*;
+import java.awt.event.*;
 
 import grpc.project.Service01.RequestDoorAccessMsg;
 import grpc.project.Service03.IncidentResponseGrpc;
 import grpc.project.Service03.IncidentResponseGrpc.IncidentResponseBlockingStub;
 
-public class Service03Client { 
+@SuppressWarnings("unused")
+public class Service03Client extends JFrame implements ActionListener  { 
+	//adding stubs
 	private static IncidentResponseGrpc.IncidentResponseBlockingStub blockingStub;
 	private static IncidentResponseGrpc.IncidentResponseStub asyncStub; 
+	// adding GUI buttons
+	private JButton button1;
+    private JButton button2;
+    
 	public static void main (String[] args) throws InterruptedException { 
 		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9091)
 		.usePlaintext()
 		.build(); 
 		//create the stubs
 		blockingStub = IncidentResponseGrpc.newBlockingStub(channel);
-		asyncStub = IncidentResponseGrpc.newStub(channel); 
-		blockAccess(); 
-		incidentResponse(); 
+		asyncStub = IncidentResponseGrpc.newStub(channel);
+		//USE GUI instead of calling the methods directly 
+		new Service03Client().gui();
+		//blockAccess(); 
+		//incidentResponse(); 
 		channel.shutdown();
 	 }
 	public static void blockAccess() {
@@ -69,6 +79,41 @@ public class Service03Client {
 		catch (RuntimeException e) {
 			e.printStackTrace();
 		}
+	}
+	public void gui() { 
+
+        // Create the two buttons
+        button1 = new JButton("Block Access");
+        button2 = new JButton("Incident Response");
+
+        // Add action listeners to the buttons
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+
+        // Add the buttons to the content pane
+        getContentPane().add(button1);
+        getContentPane().add(button2);
+
+        // Set the layout manager
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        // Set the size of the window
+        setSize(300, 200);
+
+        // Show the window
+        setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e) {
+	    if (e.getSource() == button1) {
+	        blockAccess();
+	    } else if (e.getSource() == button2) {
+	        try {
+	            incidentResponse();
+	        } catch (InterruptedException ex) {
+	            System.err.println("Error :  " + ex.getMessage());
+	            ex.printStackTrace();
+	        }
+	    }
 	}
 }
 		
