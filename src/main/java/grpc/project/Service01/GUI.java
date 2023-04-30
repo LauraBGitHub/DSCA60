305f541156ef0b4a61627e5626617e61cda84f82
LauraBGitHub <119501811+LauraBGitHub@users.noreply.github.com>
2023-04-30 13:04:44 +0100

    redoing the Servoice01 client to remove bugs

--- src/main/java/grpc/project/Service01/GUI.java
+++ src/main/java/grpc/project/Service01/GUI.java
@@ -1,58 +1,42 @@
 package grpc.project.Service01; 
+
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver; 
 
 import javax.swing.*;
+
+import grpc.project.Service01.RequestRFIDMsg.Builder;
 import grpc.project.Service01.SecurityRequestServiceGrpc;
-import grpc.project.Service01.RequestRFIDMsg; 
-import grpc.project.Service01.ResponseAccess; 
+
 
 
 public class GUI {
-    public static void main(String[] args) throws InterruptedException {
-        String hostname = "localhost";
-        if (args.length > 0) {
-            hostname = args[0];
-        }
-    
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(hostname, 8080)
-                .usePlaintext()
-                .build();
-    
-        SecurityRequestServiceGrpc.SecurityRequestServiceStub stub = SecurityRequestServiceGrpc.newStub(channel);
-    
-        /*main code  */
-        JFrame f;
-        f = new JFrame();
-        String RFID = JOptionPane.showInputDialog(f, "Enter RFID CODE");
-    
-        RequestRFIDMsg request = RequestRFIDMsg.newBuilder()
-                .setCode(RFID)
-                .build();
-    
-        StreamObserver<RequestRFIDMsg> requestObserver = stub.requestRFID(new StreamObserver<ResponseAccess>() {
-            @Override
-            public void onNext(ResponseAccess responseAccess) {
-                System.out.println("Access: " + responseAccess.getAccessResponse());
-            }
-    
-            @Override
-            public void onError(Throwable throwable) {
-                System.out.println("Error occurred: " + throwable.getMessage());
-            }
-    
-            @Override
-            public void onCompleted() {
-                System.out.println("Server has completed the request!");
-            }
-        });
-    
-        requestObserver.onNext(request);
-    
-        Thread.sleep(5000);
-    
-        channel.shutdown();
-    }
-
+   private static SecurityRequestServiceGrpc.SecurityRequestServiceBlockingStub blockingStub; 
+   public static void main (String args[]) { 
+	   ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
+	   blockingStub = SecurityRequestServiceGrpc.newBlockingStub(channel); 
+	   rfid(); 
+	   channel.shutdown(); 
+   }
+   
+   public static void rfid() { 
+	   JFrame f;
+       f = new JFrame();
+       String RFID = JOptionPane.showInputDialog(f, "Enter RFID CODE");
+       RequestRFIDMsg.Builder requestBuilder = RequestRFIDMsg.newBuilder().setCode(RFID); 
+       RequestRFIDMsg request = requestBuilder.build();
+       try {
+       ResponseAccess reponse = blockingStub.requestRFID(request); 
+       System.out.println(reponse);
+       } 
+       catch(StatusRuntimeException ex) {
+			// Print if any error/exception is generated.
+			System.out.println( ex.getMessage());
+			
+		}
+       
+   }
 } 
